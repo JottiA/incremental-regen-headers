d1a8eb84174536c9f058ec2320f54faa50f34bde
Jotti Arvanitidis <jotti.arvanitidis@channable.com>
2020-12-21 11:43:59 +0100

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <p>
+      Quiere peón término atrevimiento cucharón de estambre gaviota azada
+      levantar más húmedo asesinato cucharón interior cordaje honor picor oferta
+      albergue, muelle, floristería. Cucharón de disco gaviota huérfano
+      preocupación cucharón de masilla cacareo de rata cucharón más húmedo
+      cabaña de rata, un disco de piel pasas espinilla frío Cuchara de rata
+      podrida.
+    </p>
+  );
+};
+
+export default Home;
+
+export const getStaticProps = () => ({
+  props: {},
+  revalidate: 1,
+});
